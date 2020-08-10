07acc0b5afede14d50930c6be3b281a14d5d36fa
sangita.avghad@thoughtworks.com <sangita.avghad@thoughtworks.com>
2020-08-10 11:29:07 +0530

    784_addHospital | Sai/Sangita | Adding api to add or update the Organization

--- src/main/java/in/org/projecteka/clientregistry/controller/OrganizationController.java
+++ src/main/java/in/org/projecteka/clientregistry/controller/OrganizationController.java
@@ -1,14 +1,21 @@
 package in.org.projecteka.clientregistry.controller;
 
+import in.org.projecteka.clientregistry.model.BridgeServiceRequest;
 import in.org.projecteka.clientregistry.model.Organization;
 import in.org.projecteka.clientregistry.repository.OrganizationRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -17,7 +24,7 @@ import java.util.List;
 @AllArgsConstructor
 public class OrganizationController {
     private final OrganizationRepository organizationRepository;
-
+    private static final Logger logger = LoggerFactory.getLogger(OrganizationController.class);
     @GetMapping(produces = "application/json")
     public @ResponseBody List<Organization> getOrganizations(@RequestParam(value = "updatedSince", required = false) String updatedSince,
                                                @RequestParam(value = "offset", required = false, defaultValue = "0") int offset,
@@ -42,5 +49,16 @@ public class OrganizationController {
             throw new ResourceNotFoundException();
         }
         return org;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping
+    public void addBridge(@RequestBody BridgeServiceRequest organization){
+        try{
+            organizationRepository.addOrUpdateOrganization(organization);
+        }catch(Exception e){
+            logger.error(e.getMessage());
+            throw new BadRequest(e.getMessage());
+        }
     }
 }
